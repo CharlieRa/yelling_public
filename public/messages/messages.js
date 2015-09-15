eff9c612e4da1ec8ed0288b1181c75b688b2dd3d
CharlieRa <carlos.ramart@gmail.com>
2015-09-15 03:18:16 -0300

    -Agregado carpeta de patrones para fondos -Cambios en vista messages

--- public/messages/messages.js
+++ public/messages/messages.js
@@ -9,18 +9,48 @@
         });
         // $mdIconProvider.iconSet('content', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg');
       })
-      .controller('messagesCtrl', DemoCtrl);
+      .directive('scrollBottom', function ()
+      {
+        return {
+          scope: {
+            scrollBottom: "="
+          },
+          link: function (scope, element) {
+            scope.$watchCollection('scrollBottom', function (newValue) {
+              if (newValue)
+              {
+                $(element).scrollTop($(element)[0].scrollHeight);
+              }
+            });
+          }
+        }
+      })
+      .controller('messagesCtrl', messageCtrl);
 
-  function DemoCtrl ($scope)
+  function messageCtrl ($scope)
   {
-    $scope.messages = [{text: "hola"}];
+    var side = 'right';
+    $scope.messages = [{text: "Bienvenido!, escribe algo interesante..."}];
     // $scope.messages = [];
-    console.log("lala");
     $scope.sendMessage = function()
     {
-      console.log($scope.messages.newMessage);
-      $scope.messages.push({text: $scope.messages.newMessage});
-      $scope.formNewMessage.$setPristine();
-      $scope.messages.newMessage = "";
+      side = side == 'left' ? 'right' : 'left';
+      if(!$scope.messages.newMessage == "")
+      {
+        $scope.messages.push(
+          {
+            text: $scope.messages.newMessage,
+            side
+          });
+        $scope.formNewMessage.$setPristine();
+        $scope.messages.newMessage = "";
+      }
     }
+    $scope.user = {
+      username : 'Carlos',
+      avatar   : 'img/avatar.jpg'
+    }
+    // $(".bubble-container").animate({
+    //     bottom: $(".bubble-container").height() - $("#chatbox").height()
+    // }, 250);
   }
