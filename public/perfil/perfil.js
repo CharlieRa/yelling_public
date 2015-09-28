496738d6b8bf874883e8ed99b84cd59d539c52e9
charliera <carlos.ramart@gmail.com>
2015-09-28 17:34:03 -0300

    -Casi terminado vista perfil -Cambios en vista messages en la forma de los mensajes

--- public/perfil/perfil.js
+++ public/perfil/perfil.js
@@ -9,6 +9,22 @@ angular.module('yelling.perfil', ['ngRoute'])
   });
 }])
 
-.controller('perfilCtrl', [function() {
-
+.controller('perfilCtrl', ['$scope',function($scope) {
+  $scope.toggle = {
+    username: 'true',
+    email: 'true',
+    range: 'true',
+    gender: 'true',
+    age: 'true',
+  }
+  $scope.user = {
+    username: 'Carlos Ramart',
+    email: 'carlos.ramart@gmail.com',
+    range: 400,
+    gender: 'Masculino',
+    age: 24
+  };
+  $scope.changeAvatar = function(){
+    console.log("lala");
+  };
 }]);
