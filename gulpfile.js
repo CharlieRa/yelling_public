9ffdb2d010d502d598e74676c5de27e37f338240
CharlieRa <carlos.ramart@gmail.com>
2015-10-15 17:16:06 -0300

    -Creadas las carpetas dist y src donde se alojan css,icon,img,js con y sin procesar -Agregado gulp con su respectivo gulpfile.js en donde se agregaron task para minificar y tambn compilar scss a css -Algunos cambios en app.js  -Se boroo version.js ya que no se usa -Cambios en las distintas vistas con las imagenes que se movieron de lugar

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
+var mincss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
+/* Webserver for development */
 gulp.task('webserver', function() {
   gulp.src('public')
     .pipe(webserver({
@@ -13,10 +18,35 @@ gulp.task('webserver', function() {
     }));
 });
 
+/* Task para compilar .sass o .scss a .css*/
 gulp.task('sass', function() {
-   gulp.src('public/sass/*.scss')
+   return gulp.src('public/src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('public/src/css'));
 });
 
-gulp.task('default',['sass', 'webserver']);
+/* Task para minnificar css */
+gulp.task('mincss', function() {
+   return gulp.src('public/src/css/*.css')
+    .pipe(mincss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/dist/css'));
+});
+
+gulp.task('imgmin', function () {
+    return gulp.src('public/src/img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('public/dist/img'));
+});
+
+/* Task para minificar js */
+// gulp.task('uglifyjs', function() {
+//   return gulp.src('/*.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('public/dist/js'));
+// });
+
+gulp.task('default',['sass', 'mincss', 'imgmin', 'webserver']);
--- public/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('yell.version', [
-  'yell.version.interpolate-filter',
-  'yell.version.version-directive'
-])
-
-.value('version', '0.1');
