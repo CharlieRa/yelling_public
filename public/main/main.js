fd2ea8a25a816a724eb88eb9b75dee05b58055f6
Guillermo Labra <gmlabra@gmail.com>
2015-11-09 15:16:48 -0300

    se agrega nuevo boton de feedback

--- public/main/main.js
+++ public/main/main.js
@@ -45,7 +45,7 @@ angular
       }
     }
   })
-  .controller('mainCtrl', function ($scope, $state,$location, Auth, $mdSidenav, uiGmapGoogleMapApi, User) {
+  .controller('mainCtrl', function ($scope, $state,$location, Auth, $mdSidenav, uiGmapGoogleMapApi, User ,$mdDialog) {
     $state.transitionTo('main.messages');
     $scope.settings = [
     { name: 'Yelling!', path: 'main.messages', icon: 'dist/icons/ic_message_black_24px.svg'},
@@ -91,4 +91,57 @@ angular
     $scope.go = function(path){
       $state.transitionTo(path);
     }
+
+    $scope.showFeedback = function(ev){
+        var parentEl = angular.element(document.body);
+        $mdDialog.show({
+          controller: function($scope , $mdDialog, $http ,$mdToast)
+          {
+            $scope.submitMe = function() {
+            var comment;
+            comment = $scope.feedback.comment;
+            console.log('Enviando feedback con valor:', comment);
+            $http.post('/api/feedbacks', {
+              content: comment
+            }).success(function(){
+              $mdToast.show(
+                 $mdToast.simple()
+                   .content('Feedback enviado, gracias!')
+                   .hideDelay(5000)
+               );
+              $mdDialog.hide();
+            });
+          };
+          $scope.cancelDialog = function()
+          {
+            $mdDialog.hide();
+          }
+          },
+          clickOutsideToClose: true,
+          template:'<md-dialog flex="40" aria-label="New Feedback"> \
+                      <form ng-submit="$event.preventDefault()" name="formNewMessage" id="formNewMessage">\
+                        <md-toolbar>\
+                          <div class="md-toolbar-tools">\
+                            <h2>Nuevo Feedback</h2>\
+                          </div>\
+                        </md-toolbar>\
+                        <md-progress-linear ng-if="toggleProgress" md-mode="indeterminate"></md-progress-linear>\
+                        <md-dialog-content style="max-width:800px;max-height:810px; ">\
+                          <div class="md-dialog-content">\
+                          <md-input-container flex>\
+                              <label>Nuevo Feedback</label>\
+                              <input ng-readonly="toggleProgress" ng-model="feedback.comment" md-maxlength="200"></input>\
+                            </md-input-container>\
+                          </div>\
+                        </md-dialog-content>\
+                        <div class="md-actions" layout="row" ng-if="!toggleProgress">\
+                          <span flex></span>\
+                          <md-button type="submit" ng-click="submitMe()" class="md-primary"> Enviar </md-button>\
+                          <md-button ng-click="cancelDialog()"> Volver </md-button>\
+                        </div>\
+                      </form>\
+                    </md-dialog>',
+          targetEvent: ev,
+        });
+      }
   });
